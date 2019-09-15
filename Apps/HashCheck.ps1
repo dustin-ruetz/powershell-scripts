2fe0108bc3466a118d8f6b30953400692c064857
Dustin Ruetz <dtj.ruetz@gmail.com>
2019-09-15 16:20:14 -0400

    feat: HashCheck installer

---
+++ Apps/HashCheck.ps1
@@ -0,0 +1,61 @@
+Import-Module $PSScriptRoot/../Modules/Get-InstalledSoftware.psm1
+Import-Module $PSScriptRoot/../Modules/Get-Path.psm1 -Function Get-DownloadsPath
+Import-Module $PSScriptRoot/../Modules/Write-Title.psm1
+
+$Name = 'HashCheck'
+$Slug = $Name.ToLower()
+
+Write-Title $Name
+
+$AppInfo = Get-InstalledSoftware | Where-Object {$_.Name -Match $Name}
+If ($AppInfo.Version) {
+  $Version = $AppInfo.Version
+  Write-Host "$Slug-$Version is installed."
+}
+Else {
+  Write-Host "$Slug is not installed."
+}
+
+$GitHubUrl = 'https://github.com'
+$Webpage = Invoke-WebRequest -Uri "$GitHubUrl/gurnec/HashCheck/releases/latest" -UseBasicParsing
+
+# determine the latest version by finding <a> hrefs containing 'HashCheckSetup'
+# version format = 'major.minor.patch (MMMM YYYY)'
+$SetupFilename = 'HashCheckSetup-v*.*.*.exe'
+
+# two links - first is the setup file, last is the checksum file
+$ExeDownloadLink = $Webpage.Links.Href -Match $SetupFilename | Select-Object -First 1
+$ChecksumDownloadLink = $Webpage.Links.Href -Match $SetupFilename | Select-Object -Last 1
+
+$ExeFilename = $ExeDownloadLink.split('/') | Select-Object -Last 1
+$ChecksumFilename = $ChecksumDownloadLink.split('/') | Select-Object -Last 1
+
+$LatestAvailableVersion = $ExeFilename -Replace 'HashCheckSetup-v', '' -Replace '.exe', ''
+
+Write-Host "The latest version is $LatestAvailableVersion."
+$Proceed = Read-Host 'Proceed to download and install? (y/n)'
+
+If ($Proceed -eq 'y') {
+  Write-Host 'Downloading ...'
+
+  (New-Object System.Net.WebClient).DownloadFile(
+    "$GitHubUrl$ExeDownloadLink",
+    "$(Get-DownloadsPath)/$ExeFilename"
+  )
+  (New-Object System.Net.WebClient).DownloadFile(
+    "$GitHubUrl$ChecksumDownloadLink",
+    "$(Get-DownloadsPath)/$ChecksumFilename"
+  )
+
+  Write-Host "Installing $ExeFileName ..."
+  Invoke-Item "$(Get-DownloadsPath)/$ExeFilename"
+
+  Write-Host 'Installation complete.'
+  Read-Host 'Press enter to exit'
+  Exit
+}
+Else {
+  Exit
+}
+
+# cSpell:words hrefs
---
+++ Modules/Get-InstalledSoftware.psm1
@@ -0,0 +1,86 @@
+# generate a list of installed software
+# adapted from Anthony Howell's (i.e. ThePoShWolf) blog post and PowerShell Utilities repository
+# https://theposhwolf.com/howtos/Using-Powershell-to-get-a-list-of-installed-software-from-a-remote-computer-fast-as-lightning
+# https://github.com/ThePoShWolf/Utilities/blob/master/Misc/Get-InstalledSoftware.ps1
+
+# cSpell: disable
+
+Function Get-InstalledSoftware {
+  <#
+  Param(
+    [Alias('Computer', 'ComputerName', 'HostName')]
+    [Parameter(ValueFromPipeline=$True, ValueFromPipelineByPropertyName=$true, Mandatory=$true, Position=1)]
+    [string[]]$Name = $env:COMPUTERNAME
+  )
+  #>
+
+  Begin {
+    $lmKeys = @(
+      'Software\Microsoft\Windows\CurrentVersion\Uninstall',
+      'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+    )
+    $lmReg = [Microsoft.Win32.RegistryHive]::LocalMachine
+    $cuKeys = @('Software\Microsoft\Windows\CurrentVersion\Uninstall')
+    $cuReg = [Microsoft.Win32.RegistryHive]::CurrentUser
+  }
+
+  Process {
+    <#
+    if (!(Test-Connection -ComputerName $Computer -count 1 -quiet)) {
+      Write-Error -Message "Unable to contact $Computer. Please verify its network connectivity and try again." -Category ObjectNotFound -TargetObject $Computer
+      Break
+    }
+    #>
+
+    $masterKeys = @()
+    $remoteCURegKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($cuReg, $computer)
+    $remoteLMRegKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($lmReg, $computer)
+
+    foreach ($key in $lmKeys) {
+      $regKey = $remoteLMRegKey.OpenSubkey($key)
+      foreach ($subName in $regKey.GetSubkeyNames()) {
+        foreach($sub in $regKey.OpenSubkey($subName)) {
+          $masterKeys += (New-Object PSObject -Property @{
+            'ComputerName' = $Computer
+            'Name' = $sub.GetValue('displayname')
+            'SystemComponent' = $sub.GetValue('systemcomponent')
+            'ParentKeyName' = $sub.GetValue('parentkeyname')
+            'Version' = $sub.GetValue('DisplayVersion')
+            'UninstallCommand' = $sub.GetValue('UninstallString')
+            'InstallDate' = $sub.GetValue('InstallDate')
+            'RegPath' = $sub.ToString()
+          })
+        }
+      }
+    }
+
+    foreach ($key in $cuKeys) {
+      $regKey = $remoteCURegKey.OpenSubkey($key)
+      if ($regKey -ne $null) {
+        foreach ($subName in $regKey.getsubkeynames()) {
+          foreach ($sub in $regKey.opensubkey($subName)) {
+            $masterKeys += (New-Object PSObject -Property @{
+              'ComputerName' = $Computer
+              'Name' = $sub.GetValue('displayname')
+              'SystemComponent' = $sub.GetValue('systemcomponent')
+              'ParentKeyName' = $sub.GetValue('parentkeyname')
+              'Version' = $sub.GetValue('DisplayVersion')
+              'UninstallCommand' = $sub.GetValue('UninstallString')
+              'InstallDate' = $sub.GetValue('InstallDate')
+              'RegPath' = $sub.ToString()
+            })
+          }
+        }
+      }
+    }
+
+    $woFilter = {$null -ne $_.name -AND $_.SystemComponent -ne '1' -AND $null -eq $_.ParentKeyName}
+    $props = 'Name', 'Version', 'ComputerName', 'Installdate', 'UninstallCommand', 'RegPath'
+    $masterKeys = ($masterKeys | Where-Object $woFilter | Select-Object $props | Sort-Object Name)
+    $masterKeys
+  }
+
+  End {}
+}
+
+Export-ModuleMember Get-InstalledSoftware
